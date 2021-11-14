bba91b2cf9f971e0c8abc30d2d6a2fefd90fbddc
fiujfdssd <ariprock96@gmail.com>
2021-11-14 15:10:06 +0800

    renew

--- Prac2.py
+++
@@ -1,4 +0,0 @@
-inches = int(input("How many inches of rain have fallen: "))
-volume = (inches/12) * 43560
-gallons = volume * 7.48051945
-print(inches, " in. rain on 1 acre is", gallons, "gallons")
