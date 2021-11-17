74666643340cd78338175ad876696913090b660e
fiujfdssd <ariprock96@gmail.com>
2021-11-17 22:38:47 +0800

    Chap3&4

---
+++ week4/Prac9.py
@@ -0,0 +1,8 @@
+# open file for writing:
+# creates file if it does not exist
+# overwrite file if it exists
+
+temp_file = open("temp.txt","w")
+print("first line", file=temp_file)
+print("second line", file=temp_file)
+temp_file.close()
